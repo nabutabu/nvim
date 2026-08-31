52ff3a58913b42d207b8d2f980325b6ad640004b
Navya Sharma <navya.sharma2402@gmail.com>
2026-08-30 23:05:12 -0700

    Add nvim-treesitter with parser installs

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 require("config.lazy")
+require('nvim-treesitter').install { 'rust', 'javascript', 'zig', 'go', 'gomod', 'gosum', 'html', 'bash', 'c', 'c_sharp', 'diff', 'markdown', 'python', 'regex', 'sql', 'starlark', 'terraform', 'typescript' }
